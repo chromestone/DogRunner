dffc5897c0b509f58f091f800f2f949239b7ca1f
Derek Zhang <derekzhang@Dereks-MBP.home>
2016-10-08 12:04:50 -0400

    Pausing and better hitboxes
    Now you can pause this game!
    The hitboxes have been changed in the MainSurvivalScreen and CarSpawner
    to allow this game to be more user friendly.

--- src/org/qohs/dogrunner/screens/MainSurvivalScreen.java
+++ src/org/qohs/dogrunner/screens/MainSurvivalScreen.java
@@ -7,14 +7,11 @@ import org.qohs.dogrunner.text.TextRenderer;
 import org.qohs.dogrunner.text.mainsurvival.ScoreText;
 
 import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 /**
@@ -36,9 +33,14 @@ public class MainSurvivalScreen extends StageScreen {
 	private ClickListener upperHandler;
 	private ClickListener lowerHandler;
 	
+	private boolean gamePaused;
+	private QueryButton pauseButton;
+	private QueryButton playButton;
+	
 	private TextureRegion car;
 	private final float carHeight;
 	private final float carWidth;
+//	private final float carRatio;
 	
 	private CarSpawner carSpawner;
 	
@@ -56,8 +58,9 @@ public class MainSurvivalScreen extends StageScreen {
 		car.flip(false, true);
 		
 		//fit car into screen
-		carHeight = meterHeight / 8;
+		carHeight = meterHeight / 8f;
 		carWidth = carHeight / car.getTexture().getHeight() * car.getTexture().getWidth();
+//		carRatio = carWidth / carHeight;
 		
 		//configure screen units
 		cam = new OrthographicCamera();
@@ -68,18 +71,30 @@ public class MainSurvivalScreen extends StageScreen {
 		textRenderer = new TextRenderer(cam);
 		textRenderer.add(new ScoreText(dogRunner.assetManager.get(DogAssets.COMIC_SANS40.fileName, BitmapFont.class)));
 	
-		int upperBound = (int) ((meterHeight - (meterHeight * .25)) / 2);
-		int lowerBound = (int) (upperBound + (meterHeight * .25));
+		float upperBound = (meterHeight - (meterHeight * .25f)) / 2;
+		float lowerBound = upperBound + (meterHeight * .25f);
 		
-		ClickHandler clickHandler = new ClickHandler(0, 0, (int) meterWidth, upperBound);
+		ClickHandler clickHandler = new ClickHandler(0, 0, meterWidth, upperBound);
 		upperHandler = (ClickListener) clickHandler.getListeners().get(0);
 		
 		stage.addActor(clickHandler);
 		
-		clickHandler = new ClickHandler(0, lowerBound, (int) meterWidth, (int) (meterHeight - lowerBound));
+		clickHandler = new ClickHandler(0, lowerBound, meterWidth, meterHeight - lowerBound);
 		lowerHandler = (ClickListener) clickHandler.getListeners().get(0);
 		
 		stage.addActor(clickHandler);
+		
+		gamePaused = false;
+		
+		pauseButton = new QueryButton(meterWidth - meterHeight / 10, meterHeight / 2 - meterHeight / 20, meterHeight / 10, meterHeight / 10, 
+				new TextureRegion(dogRunner.assetManager.get(DogAssets.PAUSE_IMAGE.fileName, Texture.class)));
+		stage.addActor(pauseButton);
+		
+		playButton = new QueryButton(meterWidth / 2 - meterHeight / 2, 0f, meterHeight, meterHeight, 
+				new TextureRegion(dogRunner.assetManager.get(DogAssets.RESUME_IMAGE.fileName, Texture.class)));
+		playButton.setTouchable(Touchable.disabled);
+		playButton.setVisible(false);
+		stage.addActor(playButton);
 		
 		physicsWorld = null;
 	}
@@ -97,34 +112,56 @@ public class MainSurvivalScreen extends StageScreen {
 		MainSurvivalWorld.Definition def = new MainSurvivalWorld.Definition();
 		def.meterWidth = meterWidth;
 		def.meterHeight = meterHeight;
-		def.carWidth = carWidth;
-		def.carHeight = carHeight;
+		def.carWidth = carWidth - 7f;
+		def.carHeight = carHeight - 4f;
 		
 		physicsWorld = new MainSurvivalWorld(new Vector2(0f, 0f), true, def);
 		
-		carSpawner = new CarSpawner(physicsWorld.world, meterWidth, meterHeight, carWidth * 1.75f);//old values//1.5f//4f
+		carSpawner = new CarSpawner(physicsWorld.world, meterWidth, meterHeight, carWidth * 1.5f);//with three cars was 1.75//old values//1.5f//4f
+		
+		gamePaused = false;
 	}
 	
 	@Override
 	public void render(float delta) {
 		
-		if (physicsWorld.carCrashed) {
-			
-			dogRunner.assetManager.get(DogAssets.CAR_CRASH_BONG.fileName, Sound.class).play();
-			dogRunner.setScreen(DogScreens.Type.GAME_OVER_SCREEN);
-			return;
+		if (!gamePaused) {
+
+			act(delta);
+
+			if (physicsWorld.carCrashed) {
+
+				dogRunner.assetManager.get(DogAssets.CAR_CRASH_BONG.fileName, Sound.class).play();
+				dogRunner.setScreen(DogScreens.Type.GAME_OVER_SCREEN);
+				return;
+			}
+		}
+		else if (playButton.queryClicked()) {
+				
+			gamePaused = false;
+			playButton.setTouchable(Touchable.disabled);
+			playButton.setVisible(false);
+			pauseButton.setTouchable(Touchable.enabled);
+			pauseButton.setVisible(true);
 		}
 		
+		////////////////////////////////
+		//Draws the background roads
 		dogRunner.renderer.begin(ShapeType.Filled);
-		dogRunner.renderer.setColor(Color.BLUE);
-		dogRunner.renderer.rect(0, 0, meterWidth, meterHeight / 3);
+		dogRunner.renderer.setColor(Color.BLACK);
+		dogRunner.renderer.rect(0, 0, meterWidth, meterHeight / 6);
 		dogRunner.renderer.setColor(Color.WHITE);
-		dogRunner.renderer.rect(0, meterHeight / 3, meterWidth, meterHeight / 3);
-		dogRunner.renderer.setColor(Color.BLUE);
-		dogRunner.renderer.rect(0, meterHeight * 2 / 3, meterWidth, meterHeight / 3);
+		dogRunner.renderer.rect(0, meterHeight / 6, meterWidth, meterHeight / 6);
+		dogRunner.renderer.setColor(Color.BLACK);
+		dogRunner.renderer.rect(0, meterHeight / 3, meterWidth, meterHeight / 6);
+		dogRunner.renderer.setColor(Color.WHITE);
+		dogRunner.renderer.rect(0, meterHeight / 2, meterWidth, meterHeight / 6);
+		dogRunner.renderer.setColor(Color.BLACK);
+		dogRunner.renderer.rect(0, meterHeight * 2 / 3, meterWidth, meterHeight / 6);
+		dogRunner.renderer.setColor(Color.WHITE);
+		dogRunner.renderer.rect(0, meterHeight * 5 / 6, meterWidth, meterHeight / 6);
 		dogRunner.renderer.end();
-		
-		super.render(delta);
+		////////////////////////////////
 		
 		//exclusive or
 		if (upperHandler.isPressed() ^ lowerHandler.isPressed()) {
@@ -143,15 +180,38 @@ public class MainSurvivalScreen extends StageScreen {
 			physicsWorld.carBody.setLinearVelocity(0, 0);
 		}
 		
-		physicsWorld.render(delta);
-
+		carSpawner.render();
+		
 		dogRunner.batch.begin();
 		dogRunner.batch.draw(car, physicsWorld.carBody.getPosition().x - carWidth / 2, physicsWorld.carBody.getPosition().y - carHeight / 2, carWidth, carHeight);
 		dogRunner.batch.end();
 		
-		carSpawner.render(delta);
+		/*
+		//DEBUGGING
+		dogRunner.renderer.begin(ShapeType.Line);
+		dogRunner.renderer.rect(physicsWorld.carBody.getPosition().x - (carWidth - 7f) / 2, physicsWorld.carBody.getPosition().y - (carHeight - 4f) / 2, carWidth - 7f, carHeight - 4f);
+		dogRunner.renderer.end();
+		*/
 		
 		textRenderer.render();
+		
+		super.render(delta);
+		
+		if (pauseButton.queryClicked()) {
+			
+			gamePaused = true;
+			pauseButton.setTouchable(Touchable.disabled);
+			pauseButton.setVisible(false);
+			playButton.setTouchable(Touchable.enabled);
+			playButton.setVisible(true);
+		}
+	}
+	
+	@Override
+	protected void act(float delta) {
+		
+		physicsWorld.act(delta);
+		carSpawner.act(delta);
 	}
 
 	@Override
