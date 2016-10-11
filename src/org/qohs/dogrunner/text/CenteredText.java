1835df91991bdef005a4bafeac47594fcc02c78e
Derek Zhang <derekzhang@Dereks-MBP.home>
2016-10-11 18:45:25 -0400

    Centered Text

---
+++ src/org/qohs/dogrunner/text/CenteredText.java
@@ -0,0 +1,32 @@
+package org.qohs.dogrunner.text;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+
+/**
+ * Displays a centered text that can be set externally
+ * 
+ * @author Derek Zhang
+ *
+ */
+public class CenteredText extends TextObject {
+	
+	public String text;
+
+	public CenteredText(BitmapFont font) {
+		
+		super(font);
+		
+		text = "";
+	}
+
+	@Override
+	public void render(Batch batch) {
+
+		GlyphLayout glyphLayout = new GlyphLayout();
+		glyphLayout.setText(font, text);
+
+		font.draw(batch, glyphLayout, (dogRunner.GAME_WIDTH - glyphLayout.width) / 2, (dogRunner.GAME_HEIGHT - glyphLayout.height) / 2);
+	}
+}
