721d9cb042caea0ba8b96b0a0166dac444e3080f
Derek Zhang <derekzhang@Dereks-MBP.home>
2016-10-11 18:46:16 -0400

    Countdown Utility Class

---
+++ src/org/qohs/dogrunner/util/Countdown.java
@@ -0,0 +1,38 @@
+package org.qohs.dogrunner.util;
+
+/**
+ * Simple class that tracks time elapsed
+ * 
+ * @author Derek Zhang
+ *
+ */
+public class Countdown {
+
+	private final float startTime;
+	private float time;
+	
+	/**
+	 * 
+	 * @param startTime in seconds
+	 */
+	public Countdown(float startTime) {
+		
+		this.startTime = startTime;
+		time = startTime;
+	}
+	
+	public float update(float delta) {
+		
+		return (time -= delta);
+	}
+	
+	public float getStartTime() {
+		
+		return startTime;
+	}
+	
+	public void reset() {
+		
+		time = startTime;
+	}
+}
